68665e53eccd2c53f635d522b0a6fe8d5b658c79
Chamikara <hashanchamikara@googlemail.com>
2019-03-26 09:55:47 +0530

    Singleton Pattern

---
+++ src/creationals/singleton/SingleObject.java
@@ -0,0 +1,25 @@
+package creationals.singleton;
+
+/**
+ *
+ * @author chamikara
+ */
+public class SingleObject {
+
+    private static SingleObject instance;
+
+    private SingleObject() {
+    }
+
+    public static SingleObject getInstance() {
+        if (instance == null) {
+            instance = new SingleObject();
+        }
+        return instance;
+    }
+    
+    public void showMessage(){
+        System.out.println("Hello World!");
+    }
+
+}
